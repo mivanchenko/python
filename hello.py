b365b33f4099fdb56f1d043f2e1a683f2ffaa708
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2017-05-08 08:10:34 +0300

    First steps

---
+++ hello.py
@@ -0,0 +1,18 @@
+print("Hello, world", end=';')
+print('{0:.3f}'.format(1.0/3))
+print('{0:_^11}'.format('hello'))
+print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+age = 20
+name = 'Swaroop'
+print('{0} was {1} years old when he wrote this book'.format(name, age))
+print('{} was {} years old when he wrote this book'.format(name, age))
+
+print('This is the first line\nThis is the second line')
+print(r"Newlines are indicated by \n")
+
+s = '''This is a multi-line string.
+This is the second line.'''
+print(s)
+
+print('Perimeter is', 2)
