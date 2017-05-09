b9f0a04e2ae87cde02fdddbc1c1de8c397eba22c
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2017-05-09 12:53:33 +0300

    Control statements in python

--- hello.py
+++ hello.py
@@ -16,3 +16,74 @@ This is the second line.'''
 print(s)
 
 print('Perimeter is', 2)
+
+
+number = 23
+#guess = int(input('Your guess:'))
+guess = 23
+if guess == number:
+   # new block
+   print('Congrats!')
+elif guess < number:
+   # no
+   print('elif')
+else:
+   # no
+   print('else')
+
+print('done')
+
+
+running = False
+while running:
+   guess = int(input('Enter an integer : '))
+else:
+   print('The while loop is over.')
+
+
+for i in range(1, 5):
+    print(i)
+else:
+    print('The for loop is over')
+
+
+for i in list(range(5)):
+    print(i)
+else:
+    print('The for loop is over')
+
+
+for i in list(range(1, 5, 2)):
+    print(i)
+else:
+    print('The for loop is over')
+
+
+while True:
+#    s = input('Enter something:')
+    s = 'quit'
+    if s == 'quit':
+        break
+    print('Length of the string is', len(s))
+print('Done')
+
+
+while True:
+    s = input('Enter something : ')
+    if s == 'quit':
+        break
+    if len(s) < 3:
+        print('Too small')
+        continue
+    print('Input is of sufficient length')
+
+
+
+
+
+
+
+
+
+
+
