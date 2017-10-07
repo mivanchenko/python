8c824e451f9be9c96714fa0d204307d3bf471832
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2017-10-07 14:08:50 +0300

    Add unicode support and error handling examples

---
+++ err.py
@@ -0,0 +1,108 @@
+#try:
+#    text = input('Enter something: ')
+#except EOFError:
+#    print('Why did you do an EOF on me?')
+#except KeyboardInterrupt:
+#    print('You cancelled the operation.')
+#else:
+#    print('You entered {}'.format(text))
+
+
+#try:
+#    text = input('Enter something: ')
+#except (EOFError, KeyboardInterrupt):
+#    print('Why did you do an EOF on me?')
+#    print('You cancelled the operation.')
+#else:
+#    print('You entered {}'.format(text))
+
+
+#try:
+#    text = input('Enter something: ')
+#except:
+#    print('Something has happened')
+#else:
+#    print('You entered {}'.format(text))
+
+
+#class ShortInputException(Exception):
+#    '''A user-defined exception class.'''
+#    def __init__(self, length, atleast):
+#        Exception.__init__(self)
+#        self.length = length
+#        self.atleast = atleast
+#
+#try:
+#    text = input('Enter something: ')
+#    if len(text) < 3:
+#        raise ShortInputException(len(text), 3)
+#    # ...
+#except EOFError:
+#    print('Why did you do an EOF on me?')
+#except ShortInputException as ex:
+#    print((
+#        'ShortInputException: The input was: '
+#        + '{0} long, expected at least {1}'
+#    ).format(ex.length, ex.atleast))
+#else:
+#    print('No exception was raised.')
+
+
+#import sys
+#import time
+#
+##f = None
+#try:
+##    f.open('poem.txt')
+#    f = open('poem.txt')
+#    while True:
+#        line = f.readline()
+#        if len(line) == 0:
+#            break
+#        print(line, end='')
+#        sys.stdout.flush()
+#        print('Press ctrl+c now')
+#        time.sleep(2)
+#except IOError:
+#    print('Could not find file poem.txt')
+#except KeyboardInterrupt:
+#    print('!! You cancelled the reading from the file.')
+#finally:
+#    if f:
+#        f.close()
+#    print('(Cleaning up: closed the file)')
+#import time
+#
+##f = None
+#try:
+##    f.open('poem.txt')
+#    f = open('poem.txt')
+#    while True:
+#        line = f.readline()
+#        if len(line) == 0:
+#            break
+#        print(line, end='')
+#        sys.stdout.flush()
+#        print('Press ctrl+c now')
+#        time.sleep(2)
+#except IOError:
+#    print('Could not find file poem.txt')
+#except KeyboardInterrupt:
+#    print('!! You cancelled the reading from the file.')
+#finally:
+#    if f:
+#        f.close()
+#    print('(Cleaning up: closed the file)')
+
+
+import sys
+import time
+
+with open('poem.txt') as f:
+    for line in f:
+        if len(line) == 0:
+            break
+        print(line, end='')
+        sys.stdout.flush()
+        print('Press ctrl+c now')
+        time.sleep(2)
---
+++ uni.py
@@ -0,0 +1,22 @@
+#my_unicode = "Привіт"
+#my_unicode = "Hello"
+#my_unicode = b"Hello"
+#print(type(my_unicode))
+
+#print("\N{GREEK CAPITAL LETTER DELTA}")
+#print("\N{Latin Capital Letter Ezh}")
+#print("\u01b7")
+#print("\U000001b7")
+#print(chr(439))
+#print(ord("\N{Latin Capital Letter Ezh}"))
+
+# The opposite method of bytes.decode() is str.encode()
+
+#!/usr/bin/env python
+# -*- coding: latin-1 -*-
+
+import re
+p = re.compile('\d+', re.ASCII)
+s = "Over \u0e55\u0e57 57 flavours"
+m = p.search(s)
+print(repr(m.group()))
