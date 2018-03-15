9571dae3e2ac0fcd89e5653bec5713ae8a3b1f18
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-03-15 13:05:31 +0200

    Add default arguments for classes and 3d modelling library

--- oop2.py
+++ oop2.py
@@ -45,9 +45,52 @@ def print_attributes(obj):
     for attr in obj.__dict__:
         print(attr, getattr(obj, attr))
 
-s = Student('Mark', 29, 5)
-print(s)
-print(hasattr(s, 'age'))
-print(hasattr(s, 'ages'))
-print(s.__dict__)
-print_attributes(s)
+#s = Student('Mark', 29, 5)
+#print(s)
+#print(hasattr(s, 'age'))
+#print(hasattr(s, 'ages'))
+#print(s.__dict__)
+#print_attributes(s)
+
+
+class Kangaroo(object):
+    """a Kangaroo is a marsupial"""
+    
+    def __init__(self, contents=[]):
+        """initialize the pouch contents; the default value is
+        an empty list"""
+        self.pouch_contents = contents
+
+    def __init__(self, contents=None):
+        """initialize the pouch contents; the default value is
+        an empty list"""
+        if contents == None:
+            contents = []
+        self.pouch_contents = contents
+
+    def __str__(self):
+        """return a string representaion of this Kangaroo and
+        the contents of the pouch, with one item per line"""
+        t = [ object.__str__(self) + ' with pouch contents:' ]
+#        print('DEBUG: [' + str(self.pouch_contents) + ']')
+        for obj in self.pouch_contents:
+            s = '    ' + object.__str__(obj)
+            t.append(s)
+        return '\n'.join(t)
+
+    def put_in_pouch(self, item):
+        """add a new item to the pouch contents"""
+        self.pouch_contents.append(item)
+
+kanga = Kangaroo()
+roo = Kangaroo()
+
+kanga.put_in_pouch(roo)
+
+#print(kanga)
+#print()
+#print(roo)
+#
+#for i in range(0, 256, 51):
+#    print(i)
+#
---
+++ vp.py
@@ -0,0 +1,8 @@
+from vpython import *
+
+#scene.range = (256, 256, 256)
+#scene.center = vec(128, 128, 128)
+#color = vec(0.1, 0.1, 0.9)          # mostly blue
+#sphere(pos=scene.center, radius=128, color=color)
+
+box()
