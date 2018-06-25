3ab8843dfe1b9843a5dfd0156d8bc638baee91d8
Michael Ivanchenko <ivanchenko.michael.work@gmail.com>
2018-06-25 11:05:17 +0300

    Explain generators vs lists

--- gen.py
+++ gen.py
@@ -1,15 +1,31 @@
-return [
-    component
-    for feeds, component in COMPONENTS_MAPPING.items() if feed in feeds
-][0]
+#!/usr/bin/env python3
 
-You could do this (generator comprehension + next) to avoid iterating through the whole list if you only need the first element:
+items = [
+    [
+        ['feed1', 'feed2'], 'component12'
+    ],
+    [
+        ['feed3', 'feed4'], 'component34'
+    ],
+]
+feed = 'feed5'
 
-try:
-    return next(
-        component for feeds, component in COMPONENTS_MAPPING.items()
-        if feed in feeds
-    )
-except StopIteration:
-    raise FeedNotFound from e
+def get_list_item():
+    return [
+        component
+        for feeds, component in items if feed in feeds
+    ][0]
 
+# You could do this (generator comprehension + next) to avoid iterating
+# through the whole list if you only need the first element
+def get_gen_item():
+    try:
+        return next(
+            component
+            for feeds, component in items if feed in feeds
+        )
+    except StopIteration:
+        return 'Feed not found'
+
+#print(get_list_item())
+print(get_gen_item())
