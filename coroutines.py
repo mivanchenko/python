f9839d8f1dfa5019a203966c83e49a0174e7bb03
Michael Ivanchenko <ivanchenko.michael@gmail.com>
2018-07-04 18:22:25 +0300

    Tornado gen coroutine example

--- coroutines.py
+++ coroutines.py
@@ -1,2 +1,9 @@
 #!/usr/bin/env python3
 
+from tornado import gen
+
+@gen.coroutine
+def fetch_coroutine(url):
+    http_client = AsyncHTTPClient()
+    response = yield http_client.fetch(url)
+    return response.body
